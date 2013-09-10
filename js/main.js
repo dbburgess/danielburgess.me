e3efc4356183aba4a5c7a0dd8c24539af0ed1776
Daniel Burgess <daniel.b.burgess@gmail.com>
2013-09-09 21:41:46 -0400

    Add basic hashchange navigation for each page.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+    $(window).hashchange(function() {
+        var currentHash = location.hash ? location.hash : '#home';
 
+        $('.content-tab').not(currentHash).hide();
+        $(currentHash).show();
+    });
+
+    $(window).hashchange();
+});
